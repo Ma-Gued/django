f46522384552e0fbb3424ac96431a700f28368e5
unknown <guedonmarion@live.fr>
2024-10-08 16:49:54 +0200

    Création de la classe Poll

---
+++ items/models/poll.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question
