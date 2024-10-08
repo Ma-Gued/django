9ddbf630f2ef6f16b05357c6c6377362979f2e4d
unknown <guedonmarion@live.fr>
2024-10-08 16:56:44 +0200

    Ajout de Poll à l'admin

--- items/admin.py
+++ items/admin.py
@@ -3,7 +3,13 @@ from items.models.meal import Meal
 from items.models.logistic import Logistic
 from items.models.game import Game
 from items.models.item_to_bring import ItemToBring
+from items.models.poll import Poll
+
 
 @admin.register(Meal)
 class MealAdmin(admin.ModelAdmin):
     pass 
+
+@admin.register(Poll)
+class MealAdmin(admin.ModelAdmin):
+    pass
