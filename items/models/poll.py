30a899033161c90c2b062f0e76e7e6f8b5e3149c
unknown <guedonmarion@live.fr>
2024-10-09 15:48:16 +0200

    Ajustement de la classe Vote' avec response

--- items/models/poll.py
+++ items/models/poll.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 class Poll(models.Model):
     question = models.CharField(max_length=255)
+    # votes = models.ManyToManyField('Vote', through='UserVote')  # Association avec les votes
+
 
     def __str__(self):
         return self.question
--- items/models/vote.py
+++ items/models/vote.py
@@ -3,8 +3,14 @@ from django.contrib.auth.models import User
 from items.models import Poll, Meal
 
 class Vote(models.Model):
+    RESPONSE_CHOICES = [
+        ('oui', 'Oui'),
+        ('non', 'Non'),
+    ]
+    
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     meal = models.ForeignKey(Meal, null=True, blank=True, on_delete=models.CASCADE)
+    response = models.CharField(max_length=3, choices=RESPONSE_CHOICES, default='oui')
       
     def __str__(self):
         return f"{self.meal.name} - {self.poll.question}"
