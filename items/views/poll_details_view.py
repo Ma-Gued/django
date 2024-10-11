0e0ff5d8306205794b28d2ff206824a6c4e80236
unknown <guedonmarion@live.fr>
2024-10-11 14:23:09 +0200

    Correction de la méthode POST & Création de l'alerte ('vote pris en compte')

--- items/views/poll_details_view.py
+++ items/views/poll_details_view.py
@@ -9,44 +9,7 @@ def poll_details(request, poll_id):
     # et de renvoyer une erreur 404 si l'objet n'existe pas
     poll = get_object_or_404(Poll, id=poll_id)
     vote_options = VoteOption.objects.filter(poll=poll)
-    print("----------")
-    print(VoteOption.objects.all())
-    print(vote_options)
-    
-        # ---------------------------------
-
-    # On récupère les propositions de repas, d'intendance, de jeu et de logistique
-    # pour les afficher dans le formulaire de vote
-    # meal_proposals = Meal.objects.all()
-    # intendance_proposals = Intendance.objects.all() 
-    # game_proposals = Game.objects.all()
-    # logistic_proposals = Logistic.objects.all()
-    
-    # proposals = {
-        # 'meal': meal_proposals,
-        # 'intendance': intendance_proposals,
-        # 'game': game_proposals,
-        # 'logistic': logistic_proposals
-    # }
-    
-    # if request.method == 'POST':
-        # for proposal in proposals:
-        #     response = request.POST.get(f'proposal_{proposal.id}')
-        #     if response:
-        #         UserVote.objects.create(
-        #             user=request.user,
-        #             poll=poll,
-        #             proposal_id=proposal.id,
-        #             response=response
-        #         )
-        # return redirect('poll_results', poll_id=poll.id)
-    
-    #.get() permet de récupérer la valeur d'une clé dans un dictionnaire
-    #poll.category est la clé, et [] est la valeur par défaut si la clé n'existe pas
-    #poll.category sera remplacé par meal, intendance, game ou logistic
-    # current_proposals = proposals.get(poll.category, [])
-    # ---------------------------------
-    #
+    alert = ''
     
     if request.method == 'POST':
         for option in vote_options:
@@ -57,7 +20,6 @@ def poll_details(request, poll_id):
                     vote_option=option,
                     response=response
                 )
-        return redirect('poll_results', poll_id=poll.id)
-
-    return render(request, 'poll_votes.html', {'poll': poll, 'vote_options': vote_options})
-    # return render(request, 'poll_votes.html', {'poll': poll, 'proposals': current_proposals})
+        alert = 'Votre vote a bien été pris en compte'
+        
+    return render(request, 'poll_votes.html', {'poll': poll, 'vote_options': vote_options, 'alert': alert})
