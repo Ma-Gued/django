8764fc6c4d49d48c4dd6b38a624f2bedf25c4292
unknown <guedonmarion@live.fr>
2024-10-14 18:20:03 +0200

    Création de la view user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# clairmarais/views.py
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import User
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_list.html', {'users': users})
+
+def login(request, user_id):
+    user = User.objects.get(id=user_id)
+    request.session['user_id'] = user.id
+    request.session['user_name'] = user.first_name
+    return redirect('home')
+
+def home(request):
+    user_name = request.session.get('user_name')
+    return render(request, 'home.html', {'user_name': user_name})
