94c25acab644df5843a318b7b31bbd38a1bdb19c
unknown <guedonmarion@live.fr>
2024-10-17 23:08:45 +0200

    Debug sur poll_details (enregistrement du vote)

--- clairmarais/views/poll_details_view.py
+++ clairmarais/views/poll_details_view.py
@@ -12,13 +12,12 @@ def poll_details(request, poll_id):
     elif poll.category == 'meal':
         vote_options = VoteOption.objects.filter(poll=poll, meal__isnull=False)
     elif poll.category == 'game':
+        games = Game.objects.filter(user=request.user)
         vote_options = VoteOption.objects.filter(poll=poll, game__isnull=False)
     elif poll.category == 'logistic':
         return redirect('logistic', poll_id=poll.id)        
     else:
         vote_options = VoteOption.objects.filter(poll=poll)
-
-    games = Game.objects.filter(user=request.user)
     alert = None
 
     # Récupérer l'utilisateur connecté à partir de la session
@@ -29,14 +28,35 @@ def poll_details(request, poll_id):
 
     user = get_object_or_404(User, id=user_id)
 
-    # Récupérer les votes existants de l'utilisateur pour ce sondage
+    # Récupérer les votes de l'utilisateur pour les options de vote de ce sondage   
     user_votes = UserVote.objects.filter(user=user, vote_option__in=vote_options)
+    # Créer un dictionnaire pour accéder facilement aux votes de l'utilisateur
     user_votes_dict = {vote.vote_option.id: vote.response for vote in user_votes}
+
+    # Enregistrer les votes de l'utilisateur
+    if request.method == 'POST':
+        for option in vote_options:
+            response = request.POST.get(f'proposal_{option.id}')
+            if response:
+                try:
+                    # Vérifier si un vote existe déjà pour cet utilisateur et cette option de vote
+                    user_vote, created = UserVote.objects.get_or_create(
+                        user=user,
+                        vote_option=option,
+                        defaults={'response': response}
+                    )
+                    if not created:
+                        # Si le vote existe déjà, mettre à jour la réponse
+                        user_vote.response = response
+                        user_vote.save()
+                except IntegrityError:
+                    alert = 'Une erreur est survenue lors de la mise à jour de votre vote.'
+        return redirect('poll_details', poll_id=poll.id)
 
     return render(request, 'poll_votes.html', {
         'poll': poll,
         'vote_options': vote_options,
         'user_votes_dict': user_votes_dict,
-        'games': games,
+        
         'alert': alert
     })
